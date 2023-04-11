42e129f6546585d135a6170314c4c75e3b135922
andreas morkesdal <andreas.morkesdal@gmail.com>
2023-04-11 18:19:26 +0200

    ferdig2

--- yr/yr.go
+++ yr/yr.go
@@ -105,15 +105,85 @@ func Convert() error {
 		lineNo++
 	}
 
-	dataText := "Data er basert paa gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET); endringen er gjort av Andreas Mørkesdal,,,"
+	/*dataText := "Data er basert paa gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET); endringen er gjort av Andreas Mørkesdal,,,"
 	err = outputWriter.Write([]string{dataText})
 	if err != nil {
 		fmt.Println("Error writing data text to output file:", err)
 
+	}*/
+	dataText := `Data er basert paa gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET); endringen er gjort av Andreas Mørkesdal,,,`
+	err = outputWriter.Write([]string{dataText})
+	if err != nil {
+		fmt.Println("Error writing data text to output file:", err)
 	}
 
 	return nil
+
 }
+
+/*func Average(unit string) (float64, error) {
+	var filename string
+	var tempColumn int
+	var delimiter rune
+
+	if unit == "c" {
+		filename = "yr/kjevik-temp-celsius-20220318-20230318.csv"
+		tempColumn = 3
+		delimiter = ';'
+	} else if unit == "f" {
+		filename = "yr/kjevik-temp-fahr-20220318-20230318.csv"
+		tempColumn = 3
+		delimiter = ','
+	} else {
+		return 0, fmt.Errorf("Ugyldig verdi: %s", unit)
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.Comma = delimiter
+
+	var total float64
+	var count int
+
+	// Looper gjennom hver linje i CSV filen
+	for i := 1; ; i++ {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return 0, err
+		}
+
+		if i < 2 || i > 16755 {
+			// hopper over linjer utenfor rangen
+			continue
+		}
+
+		if len(record) <= tempColumn {
+			return 0, fmt.Errorf("Ugyldig data i filen %s", filename)
+		}
+
+		temp, err := strconv.ParseFloat(record[tempColumn], 64)
+		if err != nil {
+			return 0, err
+		}
+
+		total += temp
+		count++
+	}
+
+	if count == 0 {
+		return 0, fmt.Errorf("Ingen tempratur ble funnet i filen %s", filename)
+	}
+
+	return total / float64(count), nil
+}*/
 
 func Average(unit string) (float64, error) {
 	var filename string
@@ -161,6 +231,11 @@ func Average(unit string) (float64, error) {
 
 		if len(record) <= tempColumn {
 			return 0, fmt.Errorf("Ugyldig data i filen %s", filename)
+		}
+
+		// ignorerer linjen som inneholder informasjon om datakilden
+		if i == 16756 {
+			continue
 		}
 
 		temp, err := strconv.ParseFloat(record[tempColumn], 64)
